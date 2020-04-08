c2473868c785faad004bc353b4eaa3a66e60b121
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-04-08 21:11:07 +0200

    feat: list of cached files added

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,36 @@
 //listening to life cycle events
+
+//caching resources
+
+const cacheName = "cache-v1"
+
+const precachedResouces = [
+    "/",
+    "index.html",
+    "images/bg-intro-desktop.svg",
+    "images/bg-intro-mobile.svg",
+    "images/favicon-32x32.png",
+    "images/icon-api.svg",
+    "images/icon-budgeting.svg",
+    "images/icon-close.svg",
+    "images/icon-facebook.svg",
+    "images/icon-hamburger.svg",
+    "images/icon-instagram.svg",
+    "images/icon-onboarding.svg",
+    "images/icon-online.svg",
+    "images/icon-pinterest.svg",
+    "images/icon-twitter.svg",
+    "images/icon-youtube.svg",
+    "images/image-confetti.jpg",
+    "images/image-currency.jpg",
+    "images/image-mockups.png",
+    "images/image-plane.jpg",
+    "images/image-restaurant.jpg",
+    "images/logo.svg",
+    "scss/style.css",
+    "main.js"
+]
+
 //install event
 self.addEventListener("install", event => {
     console.log("Service worker installing");
